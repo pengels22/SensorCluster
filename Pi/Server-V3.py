fcb987e86b66517f703372d3e3f1da5486fdcbd6
AutoPush <auto@local>
2025-04-26 05:34:50 -0700

    Auto-push update: 2025-04-26 05:34:50

--- Pi/Server-V3.py
+++ Pi/Server-V3.py
@@ -741,6 +741,6 @@ if __name__ == "__main__":
 
    
 
-    socketio.run(app, host="0.0.0.0", port=5000, debug=False, use_reloader=False, server='eventlet')
+    socketio.run(app, host="0.0.0.0", port=5000)
  
 
