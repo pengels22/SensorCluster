18ec436e84e9a9a8e15f115fbf9344380d686276
AutoPush <auto@local>
2025-04-29 06:32:58 -0700

    Auto-push update: 2025-04-29 06:32:58

--- Pi/Server-V3.py
+++ Pi/Server-V3.py
@@ -19,7 +19,7 @@ from PIL import Image, ImageDraw, ImageFont, ImageOps
 import logging
 import queue
 
-
+debug = False
 
 
 SESSION_LOG = {}
@@ -282,11 +282,12 @@ def handle_logging(entry):
 
 # === SERIAL READER ===
 def serial_reader():
-    global latest_data, log_active
+    global latest_data, log_active, debug
     while True:
         try:
             line = ser.readline().decode().strip()
-            print("RAW LINE:", line)
+            if debug == True:
+                print("RAW LINE:", line)
 
             if line:
                 parts = line.split("|")
