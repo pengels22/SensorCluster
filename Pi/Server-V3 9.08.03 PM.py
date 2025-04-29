6b06256a53f43384a7d2c23a116fa727249baecd
AutoPush <auto@local>
2025-04-29 05:05:13 -0700

    Auto-push update: 2025-04-29 05:05:13

--- Pi/Server-V3 9.08.03 PM.py
+++ Pi/Server-V3 9.08.03 PM.py
@@ -400,7 +400,7 @@ def authenticate():
         digital_modes = get_current_digital_modes()
         
 
-        start_session_log(connection_type, voltage_mode, digital_modes, analog_modes)
+        start_session_log(connection_type, voltage_mode, digital_modes)
         append_session_log("✅ Authenticated", "INFO")
 
         return jsonify({"message": "Authenticated"})
