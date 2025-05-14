b586b9fda2b2b6156f1183393bb6336a77c16bde
AutoPush <auto@local>
2025-05-14 15:06:59 -0700

    Auto-push update: 2025-05-14 15:06:59

--- Pi/Server-V3.py
+++ Pi/Server-V3.py
@@ -30,7 +30,7 @@ from luma.core.interface.serial import i2c
 from luma.oled.device import sh1106
 from PIL import Image, ImageDraw, ImageFont, ImageOps
 from adafruit_ina219 import INA219
-i2c_bus = busio.I2C(board.SCL, bord.SDA)
+i2c_bus = busio.I2C(board.SCL, board.SDA)
 ina219 = INA219(i2c_bus, addr=0x40)  # Try 0x45 if needed
 
 debugg = False
